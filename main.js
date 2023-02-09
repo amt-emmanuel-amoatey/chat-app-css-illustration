722b166de80322f0cabfce77a86bc5a10983531d
Amoatey <emmanuel.amoatey@amalitech.org>
2023-02-09 18:52:15 +0000

    corrected error - renamed file

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const messages = document.getElementsByClassName("msg");
 for (let i = 0; messages.length > i; i++) {
-  setTimeout(() => messages[i].classList.add("show"), i * 1000);
+  setTimeout(() => messages[i].classList.add("show"), i * 3000);
 }
